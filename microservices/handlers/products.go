08482f9535eaf39bfbd65529c55480ac7bd9f389
rameshma19 <52102101+rameshma19@users.noreply.github.com>
2020-08-23 21:53:21 +0530

    Add files via upload

---
+++ microservices/handlers/products.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+)
+
+type Products struct {
+	l *log.Logger
+}
+
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+func (p *Products) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+
+	if req.Method == http.MethodGet {
+		p.getProducts(rw, req)
+		return
+	} else if req.Method == http.MethodPost {
+		p.addProduct(rw, req)
+		return
+	}
+	// all other case
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) getProducts(rw http.ResponseWriter, req *http.Request) {
+	p.l.Println("Handle Get Products")
+
+	lp := getProductList()
+	err := lp.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, req *http.Request) {
+	p.l.Println("Handle Post Products")
+}
+
+//Just trying git
