3d73ba3fc770e8860fc9d6f9779a033832829616
ramesh.m <ramesh.mantripragada@gmail.com>
2020-08-29 23:01:46 +0530

    vlib_3

--- vlib/data/vlibdata.go
+++ vlib/data/vlibdata.go
@@ -1,8 +1,9 @@
 package data
 
+//This is a video library struct
 type VideoLibrary struct {
-	ID       int    `json: "id"`
-	Title    string `json: "title"`
+	ID       int    `json:"id"`
+	Title    string `json:"title"`
 	Langague string `json:"lang"`
 	Link     string `json:"link"`
 }
