fa7159f239e3eb329e3b3c4640a5a901857e35a0
ramesh.m <ramesh.mantripragada@gmail.com>
2020-09-06 19:20:50 +0530

    docker-compose1

--- sitevisits/index.js
+++ sitevisits/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const redis = require('redis');
+const process = require('process');
 
 const app = express();
 const client = redis.createClient({
@@ -9,8 +10,10 @@ const client = redis.createClient({
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
+    process.exit(100);
     client.get('visits', (err, visits) => {
-        res.send('Number of visits is ', visits);
+        const newLocal = 200;
+        res.status(newLocal).send('site visits:'+visits)
         client.set('visits', parseInt(visits) + 1);
     });
 });
